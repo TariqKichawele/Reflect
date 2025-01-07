a945c4aeaa80020a09eb0183a424c6a5fe4f39b1
TariqKichawele <tariqkichawele66@gmail.com>
2025-01-07 17:33:58 -0500

    Add authentication and layout components with Clerk integration
    - Introduced middleware for route protection using Clerk.
    - Added authentication layouts for sign-in and sign-up pages.
    - Created a Header component with navigation and user authentication buttons.
    - Implemented a UserMenu for user account management.
    - Updated layout to include ClerkProvider and new styles.
    - Added background and logo images to the public directory.
    - Updated package.json and package-lock.json to include @clerk/nextjs dependency.

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Reflect",
@@ -18,10 +12,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={` ${inter.className} bg-gradient-to-b from-orange-50 via-amber-50 to-orange-50`}>
+            <div className="bg-[url('/bg.jpg')] opacity-50 fixed -z-10 inset-0 "/>
+
+            <Header />
+            <main className="min-h-screen">
+              {children}
+            </main>
+
+            <footer className="bg-orange-300 py-12 bg-opacity-10">
+              <div className="container mx-auto px-4 text-center text-gray-900">
+                <p>Made with 💗 by TK</p>
+              </div>
+            </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
