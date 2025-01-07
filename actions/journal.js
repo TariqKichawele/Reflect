030a8105df55bdee0577ce2affa41bce5f1bd8f0
TariqKichawele <tariqkichawele66@gmail.com>
2025-01-07 18:33:20 -0500

    Add journal entry functionality and mood management features
    - Introduced a new action to create journal entries with mood tracking.
    - Added a utility function to fetch images from Pixabay based on mood queries.
    - Created mood definitions and prompts for user engagement.
    - Implemented new layout and page components for the journal feature.
    - Added a Not Found page for better user experience.
    - Updated package.json and package-lock.json to include react-spinners for loading indicators.

---
+++ actions/journal.js
@@ -0,0 +1,46 @@
+'use server';
+
+import { MOODS } from "@/app/lib/moods";
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/dist/types/server";
+import { getPixabayImage } from "./public";
+import { revalidatePath } from "next/cache";
+
+export async function createJournalEntry(data) {
+    try {
+        const { userId } = await auth();
+        if (!userId) throw new Error("Unauthorized");
+
+        const user = await db.user.findUnique({
+            where: { clerkUserId: userId },
+        });
+        if (!user) throw new Error("User not found");
+
+        const mood = MOODS[data.mood.toUpperCase()];
+        if (!mood) throw new Error("Invalid mood");
+
+        const moodImageUrl = await getPixabayImage(data.moodQuery);
+
+        const entry = await db.entry.create({
+            data: {
+                title: data.title,
+                content: data.content,
+                mood: mood.id,
+                moodScore: mood.score,
+                moodImageUrl,
+                userId: user.id,
+                collectionId: data.collectionId || null,
+            },
+        });
+
+        await db.draft.deleteMany({
+            where: { userId},
+        });
+
+        revalidatePath("/dashboard");
+
+        return entry;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
